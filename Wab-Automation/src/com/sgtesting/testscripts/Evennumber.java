02106139818779304d444109172b74dee34da46a
Karthikgowdacp <karthikcpgowda@gmail.com>
2021-11-06 17:22:54 +0530

    Evennumbe

---
+++ Wab-Automation/src/com/sgtesting/testscripts/Evennumber.java
@@ -0,0 +1,15 @@
+package com.sgtesting.testscripts;
+
+public class Evennumber {
+	public static void main(String[] args) 
+	{
+	int sum = 0;
+	int num;
+	num=10;
+	for(int i=1;i<=num;i++)
+	 {
+	 sum=sum+i;
+	 }
+	 System.out.println("="+sum);
+	        }
+	    }
